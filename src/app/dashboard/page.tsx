a62382c7e288fe7232011149b63ab3a7ff13c9ea
Lobe <lo.musonda@pacra.org.zm>
2024-11-20 12:54:28 +0200

    Prisma setup

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const DashboardPage = () => {
+  // Authentication
+
+  // If account does not exist
+  return <div>Dashboard</div>;
+};
+
+export default DashboardPage;
