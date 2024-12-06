ea6ebd99510406a926480feb8e3898972919c1b0
Lobe <lobe350@gmail.com>
2024-12-06 16:21:55 +0200

    completed change video location

--- src/components/forms/change-video-location/index.tsx
+++ src/components/forms/change-video-location/index.tsx
@@ -1,5 +1,8 @@
+import Loader from '@/components/global/loader';
+import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
 import { Separator } from '@/components/ui/separator';
+import { Skeleton } from '@/components/ui/skeleton';
 import { useMoveVideos } from '@/hooks/use-folders';
 import React from 'react';
 
@@ -32,7 +35,7 @@ const ChangeVideoLocation = ({
   );
 
   return (
-    <form className='flex flex-col gap-y-5'>
+    <form className='flex flex-col gap-y-5' onSubmit={onFormSubmit}>
       <div className='border rounded-xl p-5'>
         <h2 className='text-xs text-[#a4a4a4]'>Current Workspace</h2>
         {workspace && <p>{workspace.name}</p>}
@@ -44,13 +47,64 @@ const ChangeVideoLocation = ({
         <h2 className='text-xs text-[#a4a4a4]'>To</h2>
         <Label className='flex flex-col gap-y-2'>
           <p className='text-xs'>Workspace</p>
-          <select className='rounded-xl text-base bg-transparent'>
-            <option className='text-[#a4a4a4]' value={'1'}>
-              Workspace 1
-            </option>
+          <select
+            className='rounded-xl text-base bg-transparent'
+            {...register('workspace_id')}
+          >
+            {workspaces.map((space) => (
+              <option
+                key={space.id}
+                value={space.id}
+                className='text-[#a4a4a4]'
+              >
+                {space.name}
+              </option>
+            ))}
           </select>
         </Label>
+        {isFetching ? (
+          <Skeleton className='w-full h-[40px] rounded-xl' />
+        ) : (
+          <Label className='flex flex-col gap-y-2'>
+            <p className='text-xs'>Folders in this workspace</p>
+            {isFolders && isFolders.length > 0 ? (
+              <select
+                {...register('folder_id')}
+                className='rounded-xl bg-transparent text-base'
+              >
+                {isFolders.map((folder, key) =>
+                  key === 0 ? (
+                    <option
+                      key={folder.id}
+                      className='text-[#a4a4a4]'
+                      value={folder.id}
+                    >
+                      {folder.name}
+                    </option>
+                  ) : (
+                    <option
+                      key={folder.id}
+                      className='text-[#a4a4a4]'
+                      value={folder.id}
+                    >
+                      {folder.name}
+                    </option>
+                  )
+                )}
+              </select>
+            ) : (
+              <p className='text-[#a4a4a4] text-sm'>
+                This workspace has no folders
+              </p>
+            )}
+          </Label>
+        )}
       </div>
+      <Button>
+        <Loader state={isPending} color='#000'>
+          Transfer
+        </Loader>
+      </Button>
     </form>
   );
 };
