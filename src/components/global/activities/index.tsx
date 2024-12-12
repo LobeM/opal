80f6c7f2aa1d48669d6dba16262f505ee61bf1bc
Lobe <lobe350@gmail.com>
2024-12-12 11:42:01 +0200

    implemented comments and replies

--- src/components/global/activities/index.tsx
+++ src/components/global/activities/index.tsx
@@ -1,6 +1,10 @@
 import CommentForm from '@/components/forms/comment-form';
 import { TabsContent } from '@/components/ui/tabs';
 import React from 'react';
+import CommentCard from '../comment-card';
+import { useQueryData } from '@/hooks/use-query-data';
+import { getVideoComments } from '@/actions/user';
+import { VideoCommentProps } from '@/types/index.type';
 
 type Props = {
   author: string;
@@ -8,9 +12,30 @@ type Props = {
 };
 
 const Activities = ({ author, videoId }: Props) => {
+  const { data } = useQueryData(['video-comments'], () =>
+    getVideoComments(videoId)
+  );
+
+  const { data: comments } = data as VideoCommentProps;
+
   return (
     <TabsContent value='Activity' className='rounded-xl flex flex-col gap-y-5'>
       <CommentForm author={author} videoId={videoId} />
+      {comments?.map((comment) => (
+        <CommentCard
+          key={comment.id}
+          comment={comment.comment}
+          author={{
+            image: comment.User?.image!,
+            firstname: comment.User?.firstname!,
+            lastname: comment.User?.lastname!,
+          }}
+          videoId={videoId}
+          commentId={comment.id}
+          reply={comment.reply}
+          createdAt={comment.createdAt}
+        />
+      ))}
     </TabsContent>
   );
 };
